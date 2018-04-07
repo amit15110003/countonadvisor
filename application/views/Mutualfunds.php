3ae84c21e01bd5a17f0e5fd71adebe78cf492906
rrptm <raviranjan.ptm@gmail.com>
2018-04-07 17:27:48 +0530

    llm

---
+++ application/views/Mutualfunds.php
@@ -0,0 +1,69 @@
+<div class="row page-banner" style="background-image: url('<?php echo base_url();?>media/image/contact.jpg');background-position: center;background-size: cover;background-repeat: no-repeat;">
+	<div class="container">
+		<h1 class="th-title text-center">Mutual Funds</h1>
+	</div>
+</div>
+<div class="row">
+	<div class="container">
+		<h3 class="th-htextcont">What is Mutual Fund?</h3>
+		<p class="th-textcont">A mutual fund is simply a financial intermediary that allows a group of investors to pool their money together with a predetermined investment objective. The mutual fund will have a fund manager who is responsible for investing the pooled money into specific securities (usually stocks or bonds). Mutual funds are one of the best investments ever created because they are very cost efficient and very easy to invest in (you don't have to figure out which stocks or bonds to buy).</p>
+
+		<h3 class="th-htextcont">Advantages of Mutual Fund:</h3>
+		<p class="th-textcont"><b>Professional Management:</b> The primary advantage of funds is the professional management of your money. Investors purchase funds because they do not have the time or the expertise to manage their own portfolio. A mutual fund is a relatively inexpensive way for a small investor to get a full-time manager to make and monitor the investments.<br><br> 
+
+		<b>Diversification:</b> By owning "shares"(known as "units") in a mutual fund instead of owning individual stocks or bonds, your risk is spread out. The idea behind diversification is to invest in a number of assets so that a loss in any particular investment is minimized by gains in others. In other words, the more stocks and bonds you own, the less any one of them can hurt you. Large mutual funds typically own hundreds of different stocks in many different industries. It wouldn't be possible for a small investor to build this kind of portfolio with a small amount of money. <br><br>
+
+		<b>Economies of Scale:</b> Because a mutual fund buys and sells large amounts of securities at a time, its transaction costs are lower than you as an individual would pay.<br><br> 
+
+		<b>Liquidity:</b> Just like an individual stock, a mutual fund allows you to sell the units at any time.<br><br> 
+
+		<b>Simplicity:</b> Buying a mutual fund is easy! The minimum investment is also very small. As little as Rs 500 can be invested on a monthly basis. Just contact us to know more.<br><br></p>
+
+		<h3 class="th-htextcont">History of Mutual Fund:</h3>
+		<p class="th-textcont">The origin of mutual fund industry in India was with the introduction of the concept of mutual fund by UTI in the year 1963. It accelerated from the year 1987 when non-UTI players entered the industry. In the past decade, Indian mutual fund industry had seen a dramatic imporvements, both qualitywise as well as quantitywise.</p>
+
+		<h3 class="th-htextcont">Types of Funds:</h3>
+		<p class="th-textcont">»  Open Ended <br>
+			»  Close Ended<br></p>
+
+		<h3 class="th-htextcont">Classification of Funds:</h3>
+		<p class="th-textcont"></p>
+					<table>
+						  <tr>
+						    <th>Company</th>
+						    <th>Contact</th>
+						    <th>Country</th>
+						  </tr>
+						  <tr>
+						    <td>Alfreds Futterkiste</td>
+						    <td>Maria Anders</td>
+						    <td>Germany</td>
+						  </tr>
+						  <tr>
+						    <td>Centro comercial Moctezuma</td>
+						    <td>Francisco Chang</td>
+						    <td>Mexico</td>
+						  </tr>
+						  <tr>
+						    <td>Ernst Handel</td>
+						    <td>Roland Mendel</td>
+						    <td>Austria</td>
+						  </tr>
+						  <tr>
+						    <td>Island Trading</td>
+						    <td>Helen Bennett</td>
+						    <td>UK</td>
+						  </tr>
+						  <tr>
+						    <td>Laughing Bacchus Winecellars</td>
+						    <td>Yoshi Tannamuri</td>
+						    <td>Canada</td>
+						  </tr>
+						  <tr>
+						    <td>Magazzini Alimentari Riuniti</td>
+						    <td>Giovanni Rovelli</td>
+						    <td>Italy</td>
+						  </tr>
+					</table>
+	</div>
+</div>
